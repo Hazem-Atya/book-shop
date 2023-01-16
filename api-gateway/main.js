a5984f86627ee1efaaeac529d233920550043d95
hazem-atya <hazematya1@gmail.com>
2023-01-16 01:20:40 +0100

    edited main.js

--- api-gateway/main.js
+++ api-gateway/main.js
@@ -3,7 +3,6 @@ const app = express();
 const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
-const mongoose = require("mongoose");
 require("dotenv").config();
 const axios = require("axios");
 app.use(morgan("dev"));
